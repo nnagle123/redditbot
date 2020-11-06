bf7fc995c0a197064581a47a6762683d7e2011bc
nnalge123 <nathannagle1@gmail.com>
2020-11-06 15:13:34 -0700

    stopped the bot from replying to comments more than once.

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,6 +1,7 @@
 import praw
 import config
 import time
+import os
 
 def bot_login():
     print "Logging in..."
@@ -12,20 +13,40 @@ def bot_login():
     print "Logged in!"
     return r
 
-def run_bot(r):
+def run_bot(r, comments_replied_to):
     print "Obtaining 5 comments..."
+
+   
+
     for comment in r.subreddit('test').comments(limit=5):
-        if "dog" in comment.body:
+        if "dog" in comment.body and comment_id not in comments_replied_to and not comment_author == r.user.me():
             print "String found!"
             comment.reply("I also love dogs!")
+            print "Replied to comment" + comment.id
+
+            comments_replied_to.append(comment.id)
+
+            with open ("comments_replied_to.txt", "a") as f:
+                f.write(comment.id + "\n")
 
     print "Sleeping for 10 seconds..."
     #Sleep for 10 seconds...
     time.sleep(10)
 
-while True:
+def get_saved_comments():
+    if not os.path.isfile("comments_replied_to.txt")
+        comments_replied_to = []
+    else:
+    with open("comments_replied_to.txt", "r") as f:
+        comments_replied_to = f.read()
+        comments_replied_to - comments_replied_to.split("\n")
+        comments_replied_to = filter(None, comments_replied_to)
+
 r = bot_login()
-run_bot(r)
+comments_replied_to = []
+
+while True:
+run_bot(r, comments_replied_to)
 
 
 
